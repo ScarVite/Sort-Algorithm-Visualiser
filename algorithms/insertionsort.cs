8f640f2788b757df073bebe732b9c99534c9ee91
ScarVite <43887376+ScarVite@users.noreply.github.com>
2021-06-07 17:51:04 +0200

    added insetionsort

---
+++ algorithms/insertionsort.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Algorithm_Visualiser.algorithms
+{
+    class insertionsort : sortalgorithm
+    {
+
+        public insertionsort(int[] arr)
+        {
+            if (arr.Length < 5)
+            {
+                throw new Exception("Array has too few Entries");
+            }
+            toSort = arr;
+        }
+
+        public override int sort()
+        {
+            watch.Start();
+            int totalSorts = 0;
+            //int sortedIndex = 1;
+            for (int i = 1; i <= toSort.Length - 1; i++)
+            {
+                if (toSort[i-1] > toSort[i])
+                {
+                    int temp = toSort[i];
+                    for (int b = i - 1;b > 0; b--)
+                    {
+                        if (toSort[b] > temp)
+                        {
+                            toSort[b + 1] = toSort[b];
+                        }
+                        else
+                        {
+                            toSort[b] = temp;
+                            b = -1;
+                        }
+                    }
+                    totalSorts++;
+                }
+                //sortedIndex++;
+            }
+            watch.Stop();
+            foreach (int i in toSort)
+            {
+                Console.WriteLine(i);
+            }
+            return 1;
+        }
+    }
+}
