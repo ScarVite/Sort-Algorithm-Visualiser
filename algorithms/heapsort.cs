fd641e189e0def1774f5a4c62f34fa98ccf10436
ScarVite <43887376+ScarVite@users.noreply.github.com>
2021-06-08 21:22:29 +0200

    added heapsort

---
+++ algorithms/heapsort.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Algorithm_Visualiser.algorithms
+{
+    class heapsort : sortalgorithm
+    {
+
+        /// <summary>
+        /// The Constructor for the Heapsort Algorithm
+        /// </summary>
+        /// <param name="arr">The Array to sort</param>
+        public heapsort(int[] arr)
+        {
+            if (arr.Length < 5)
+            {
+                throw new Exception("Array has too few Entries");
+            }
+            toSort = arr;
+        }
+
+        protected override int sort()
+        {
+            //Build the Initial max-heap (rearranging the array)
+            for(int i = toSort.Length/2-1; i >= 0; i--) 
+            {
+                max_heapyfy(toSort.Length, i);
+            }
+
+            //rebuild the max-heap for the non sorted keys
+            for (int i = toSort.Length-1; i> 0; i--)
+            {
+                swap(0, i);
+                max_heapyfy(i, 0);
+            }
+            return 1;
+        }
+
+        /// <summary>
+        /// Max-Heapifys the Array
+        /// </summary>
+        /// <param name="length">The Last Index of the Array to heapify</param>
+        /// <param name="large">The Largest Key</param>
+        protected void max_heapyfy(int length,int large)
+        {
+            int largest = large;
+            int l = 2 * large + 1; // left = 2*i + 1 //Some Formula i found which seems to find the Left Child
+            int r = 2 * large + 2; // right = 2*i + 2 //The Same but with right Node
+
+            if (l < length && toSort[l] > toSort[largest]) largest = l; //Check if Left Child Node's key is Larger and if, set it's Index As Largest // And check if it is larger than the length
+            if (r < length && toSort[r] > toSort[largest]) largest = r; // The Same for the Right Child Node
+
+            if(largest != large)
+            {
+                swap(large, largest); //When the Parent isn't the Largest Node Swap them, so the Paren't now is the Largest
+                max_heapyfy(large, largest); //Do The Same for the next Nodes
+            }
+        }
+    }
+}
