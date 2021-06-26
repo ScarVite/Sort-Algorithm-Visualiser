050c5a30de14a1213f42d296849da663ece97e2e
ScarVite <43887376+ScarVite@users.noreply.github.com>
2021-06-26 23:10:41 +0200

    added Selectionsort

---
+++ algorithms/Selectionsort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Algorithm_Visualiser.algorithms
+{
+    class Selectionsort : Sortalgorithm
+    {
+
+        public Selectionsort(int[] arr)
+        {
+            if (arr.Length < 5)
+            {
+                throw new Exception("Array has too few Entries");
+            }
+            toSort = arr;
+        }
+
+        protected override int sort()
+        {
+            for(int i = 0; i < toSort.Length; i++)
+            {
+                int minInd = i;
+                for (int a = i; a < toSort.Length; a++)
+                {
+                    if (toSort[minInd] > toSort[a]) minInd = a;
+                }
+                if(minInd != i) swap(minInd, i);
+            }
+            return 1;
+        }
+
+    }
+}
