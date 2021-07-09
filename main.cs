22a115a7a2e74dd80af3773af60fb08cb77d646d
ScarVite <43887376+ScarVite@users.noreply.github.com>
2021-07-09 20:05:44 +0200

    removed tast case

--- main.cs
+++ main.cs
@@ -29,28 +29,7 @@ namespace Sort_Algorithm_Visualiser
         /// </summary>
         public static void init()
         {
-            //SortHelper helper = new SortHelper();
-            //int[] arrToSort = helper.getArray(5);
-            //Radixsort sort = new Radixsort(arrToSort);
-            //sort.startSort();
-            //test();
-        }
 
-        private async static void test()
-        {
-            SortHelper helper = new SortHelper();
-            for (int i = 5; i > 0 ; i--)
-            {
-                int[] arrToSort = helper.getArray(10);
-                Shellsort sort = new Shellsort(arrToSort, mainGui);
-                int[] sorted = await sort.startSort();
-                bool sortedB = true;
-                for(int a = 1; a < sorted.Length;a++)
-                {
-                    if (sorted[a - 1] > sorted[a]) sortedB = false;
-                }
-                Console.WriteLine(sortedB);
-            }
         }
     }
 }
