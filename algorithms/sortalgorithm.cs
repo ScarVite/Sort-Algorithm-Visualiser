04a8b8c0768b1863265d0c1711080291838fddc7
ScarVite <43887376+ScarVite@users.noreply.github.com>
2021-07-09 18:09:07 +0200

    added getters, cancel token and gui udpater

--- algorithms/sortalgorithm.cs
+++ algorithms/sortalgorithm.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using Sort_Algorithm_Visualiser;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
-using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,23 +11,59 @@ namespace Sort_Algorithm_Visualiser.algorithms
     abstract class Sortalgorithm
     {
 
-        protected Stopwatch watch = new Stopwatch();
+        protected CancellationTokenSource tokenSource = new CancellationTokenSource();
+        protected CancellationToken token;
         protected int[] toSort;
         protected bool sorted = false;
         protected int totalSorts = 0;
+        protected gui mainGui;
+        private int result = 0;
+
+        protected Sortalgorithm(gui gui)
+        {
+            mainGui = gui;
+        }
 
         /// <summary>
         /// Starts The Sort Algorithm.
         /// </summary>
         public async Task<int[]> startSort()
         {
-            int result = 0;
-            watch.Start();
-            await Task.Run(() => result = sort());
-            if (result != 1) Console.WriteLine("CERNEL PAGE FAULT, CODE: {0}", result); 
-            watch.Stop();
-            Console.WriteLine("Array: [{0}]; TotalSorts: {1}; Time: {2}", string.Join(", ", toSort), totalSorts, watch.Elapsed.TotalMilliseconds);
+            token = tokenSource.Token;
+            await Task.Run(() => result = sort(), token);
+            if (result != 1) Console.WriteLine("CERNEL PAGE FAULT, CODE: {0}", result);
+            Console.WriteLine("Array: [{0}]; TotalSorts: {1}; ", string.Join(", ", toSort), totalSorts);
             return toSort;
+        }
+
+        public void newArray(int[] arr)
+        {
+            toSort = arr;
+        }
+
+
+        /// <returns>Time it Took to Sort the Array</returns>
+
+        /// <returns>The Array, either Sorted or Unsorted</returns>
+        public int[] getArray()
+        {
+            return toSort;
+        }
+
+        public void killTask()
+        {
+            tokenSource.Cancel();
+        }
+
+        /// <returns>Amount of Sorts, it took</returns>
+        public int getSorts()
+        {
+            return totalSorts;
+        }
+
+        public int getStatusCode()
+        {
+            return result;
         }
 
         /// <summary>
@@ -40,6 +77,8 @@ namespace Sort_Algorithm_Visualiser.algorithms
             int temp = toSort[i];
             toSort[i] = toSort[j];
             toSort[j] = temp;
+            mainGui.update(toSort, totalSorts);
+            Thread.Sleep(1000);
         }
 
         /// <summary>
