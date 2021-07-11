45262814dd99e2e96f2317e548ff3d97668ebabd
ScarVite <43887376+ScarVite@users.noreply.github.com>
2021-07-11 16:38:24 +0200

    Add Assembly Info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sort-Algorithm-Visualiser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Visualizer for Sorting Algorithms")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Sort-Algorithm-Visualiser")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("ScarVIte ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
