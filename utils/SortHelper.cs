8f80f9d1fddc07a03c66cc352f4ddad5f6b30e7a
ScarVite <43887376+ScarVite@users.noreply.github.com>
2021-07-11 16:20:36 +0200

    Added Special Cases

--- utils/SortHelper.cs
+++ utils/SortHelper.cs
@@ -17,12 +17,44 @@ namespace Sort_Algorithm_Visualiser.utils
         /// <param name="min_value">The Minimal Value for a Value</param>
         /// <param name="max_value">The Maximum Value for a Value</param>
         /// <returns>Array containing random numbers</returns>
-        public int[] getArray(int length, int min_value, int max_value)
+        public int[] getRandArray(int length, int min_value, int max_value)
         {
             int[] toSort = new int[length];
             for (int i = 0; i < length; i++)
             {
                 toSort[i] = rand.Next(min_value, max_value);
+            }
+            //Console.WriteLine("Unsorted: [{0}]", string.Join(", ", toSort));
+            return toSort;
+        }
+
+        /// <summary>
+        /// Generates an Already Sorted Array
+        /// </summary>
+        /// <param name="length">The length of the Array</param>
+        /// <returns>A Already Sorted Array</returns>
+        public int[] getSortedArray(int length)
+        {
+            int[] toSort = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                toSort[i] = i;
+            }
+            //Console.WriteLine("Unsorted: [{0}]", string.Join(", ", toSort));
+            return toSort;
+        }
+
+        /// <summary>
+        /// Generates an Reverse Sorted Array (Highest First, Smallest Last)
+        /// </summary>
+        /// <param name="length">The length of the Array</param>
+        /// <returns>The Reverse Sorted Array</returns>
+        public int[] getReverseSortedArray(int length)
+        {
+            int[] toSort = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                toSort[i] = length-i;
             }
             //Console.WriteLine("Unsorted: [{0}]", string.Join(", ", toSort));
             return toSort;
